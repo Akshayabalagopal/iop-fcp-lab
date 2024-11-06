27bbd621d0e63e2d928b89d7b092c7232f2cf243
Akshayabalagopal <i24ai001@aid.svnit.ac.in>
2024-11-07 00:26:28 +0530

    Update akshaya59.c

--- akshaya59.c
+++ akshaya59.c
@@ -1,27 +1,32 @@
-#include<stdio.h>
 int main()
 {
-	int i, a[10], b[10];
+	int num,i, a[10], b[10];
 	printf("Elements of a array\n");
-	for(i=1; i<=10;i++)
+	for(i=0; i<10; i++)
 	{
 		printf("Enter the no\n");
 		scanf("%d", &a[i]);
 	}
 	printf("Elements of b array\n");
-	for(i=1; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		printf("Enter the no\n");
 		scanf("%d", &b[i]);
 	}
+	for(i=0; i<10; i++)
+	{
+		num= a[i];
+		a[i]= b[i];
+		b[i]= num;
+	}
 	printf("Elements of a array after swap");
-	for(i=1; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		printf("%d\t", a[i]);
 	}
 	printf("\n");
 	printf("Elements of b array after swap");
-	for(i=1; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		printf("%d\t", b[i]);
 	}
