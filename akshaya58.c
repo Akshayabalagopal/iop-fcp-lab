836dada26878362d68c6227edf292890b51a4d4f
Akshayabalagopal <i24ai001@aid.svnit.ac.in>
2024-11-07 00:01:59 +0530

    Update akshaya58.c

--- akshaya58.c
+++ akshaya58.c
@@ -3,19 +3,19 @@ int main()
 {
 	int a[10], i, b[10], sum[10];
 	printf("Elements of a array\n");
-	for(i=1; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		printf("Enter the no\n");
 		scanf("%d", &a[i]);
 	}
 	printf("Elements of b array\n");
-	for(i=1; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		printf("Enter the no\n");
 		scanf("%d", &b[i]);
 	}
 	printf("Elements of sum array\n");
-	for(i=2; i<=10; i++)
+	for(i=0; i<10; i++)
 	{
 		sum[i]=a[i]+b[i];
 		printf("%d", sum[i]);
